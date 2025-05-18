bc6d2b48977069ede307364ce7ba11577b13601c
DESKTOP-TE9JGFD\danie <danieljjs77@gmail.com>
2025-05-19 02:26:35 +0900

    reviews1 update

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'region', 'created_at')
+    search_fields = ('title', 'author__username')
